537d122f0808a88cd66facec4bda5af390d4d1e5
Nicholas Hernandez <nicholashernandez39@gmail.com>
2018-01-17 14:03:06 -0800

    Added personal keys, updated client for API

--- app/models/message_creator.rb
+++ app/models/message_creator.rb
@@ -1,8 +1,13 @@
 class MessageCreator
+  require 'twilio-ruby'
   attr_accessor :message, :sms_record
 
   def initialize(params)
     @message = Message.new(allowed_params(params))
+    account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+
   end
 
   def ok?
